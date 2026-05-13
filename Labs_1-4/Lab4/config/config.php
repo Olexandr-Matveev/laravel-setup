c48f4a21f3e51afaa9c942182db3b4d0e296b30e
Olexandr-Matveev <166984863+Olexandr-Matveev@users.noreply.github.com>
2026-05-13 11:27:15 +0200

    Update config.php

--- Labs_1-4/Lab4/config/config.php
+++ Labs_1-4/Lab4/config/config.php
@@ -4,6 +4,6 @@
 
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'S1357R');
+define('DB_PASSWORD', 'Ваш пароль');
 define('DB_NAME', 'users_db');
 define('DB_PORT', 3306);
