1abcd5c912def23a0e43133e0abfe726dd4df951
Olexandr-Matveev <166984863+Olexandr-Matveev@users.noreply.github.com>
2026-05-13 12:11:09 +0200

    Fix Lab2 README and Lab4 config

--- Labs_1-4/Lab4/config/config.php
+++ Labs_1-4/Lab4/config/config.php
@@ -4,6 +4,10 @@
 
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
+<<<<<<< HEAD
 define('DB_PASSWORD', 'ВАШ_ROOT_ПАРОЛЬ');
+=======
+define('DB_PASSWORD', 'Ваш пароль');
+>>>>>>> c28e863 (Fix Lab2 README and Lab4 config)
 define('DB_NAME', 'users_db');
 define('DB_PORT', 3306);
